f37db064ff6fa4ff256c6d20b5ffc9d201444091
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-03-24 13:57:52 +0900

    update: lint rules
    - fix failing lint code

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ class App extends React.Component<{}, State> {
         <label>입력할 텍스트 : </label>
         <input type="text"
                value={this.state.text}
-               onChange={event => this.setState({text : event.target.value})}
+               onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.setState({text : event.target.value})}
                placeholder="텍스트입력해랑"
                onBlur={this.onBlurOutFocus}/>
         <p>입력한 텍스트 : {this.state.text}</p>
