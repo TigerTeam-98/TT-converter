fd308dbd4d8e3f81e21073e566b93bcaa4a4ed57
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-05-04 14:28:17 +0900

    Fix: delete removed dependency code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { Helmet } from "react-helmet";
-
-const Document = () => (
-  <Helmet>
-    <style>{`
-      body, #app {
-        margin: 0;
-        padding: 0;
-        font-family: sans-serif;
-      }
-    `}</style>
-  </Helmet>
-);
 
 const Wrapper = styled.div`
 `;
 
 export const Layout = (props: { children: React.ReactNode }) => {
   return <>
-    <Document />
     <Wrapper>
       {props.children}
     </Wrapper>
