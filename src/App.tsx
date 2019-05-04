46401dfd4e91d104f4f432f1aa5c7345782a0c50
Jake Jaewook Ahn <ajw4586@gmail.com>
2019-05-04 14:18:31 +0900

    Fix: update tslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import * as Page from "./pages";
 import { Layout } from "./components";
-import "./App.css";
 import "antd/dist/antd.css";
 
 export const App = () => (
